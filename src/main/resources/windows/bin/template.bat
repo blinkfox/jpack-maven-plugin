678e20203c8f167d015e2a3b662d06671c253afc
chenjiayin <chenjiayin@thunisoft.com>
2019-05-06 19:10:38 +0800

    Windows 下的打包修改

--- src/main/resources/windows/bin/template.bat
+++ src/main/resources/windows/bin/template.bat
@@ -1,6 +1,4 @@
 @echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 %~dp0${name}.exe ${batName}
-echo ${name} 服务当前状态:
-%~dp0${name}.exe status
 pause
