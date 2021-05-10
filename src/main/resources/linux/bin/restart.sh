c04deb36d4b535c6903eb096879a381bd0fb6c53
chenjiayin <chenjiayin@thunisoft.com>
2021-05-10 14:32:05 +0800

    修改了 Linux 系统中的多个 sh 文件

--- src/main/resources/linux/bin/restart.sh
+++ src/main/resources/linux/bin/restart.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 #=====================================================================
-# 项目重启的 shell 脚本;
-# 先调用 stop.sh 停服;
-# 然后调用 start.sh 启动服务.
+# 描述: 重启服务的 shell 脚本;
+# 步骤: 
+#     1. 先调用 stop.sh 停服;
+#     2. 由于服务可能是“优雅停机”，再循环判断服务是否真的已停止;
+#     3. 停止之后，再调用 start.sh 启动服务.
 #=====================================================================
 
 # 项目名称.
@@ -17,14 +19,15 @@ bash \${BIN_PATH}/stop.sh
 
 # 如果是优雅停机，就要循环判断服务是否关闭完成.
 bash \${BIN_PATH}/status.sh
-while [ $? -eq 1 ]
+while [ $? -eq 0 ]
 do
-    echo "\${APPLICATION} 服务还未停止"
+    echo "\${APPLICATION} 服务仍在运行中 ..."
     sleep 1s
     bash \${BIN_PATH}/status.sh
 done
 
 # 启动服务.
-echo 正在启动 \${APPLICATION} 服务...
+echo 正在启动 \${APPLICATION} 服务 ...
 bash \${BIN_PATH}/start.sh
 exit 0
+
